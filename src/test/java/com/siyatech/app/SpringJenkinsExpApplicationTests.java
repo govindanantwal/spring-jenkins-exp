3b54bf8e3b606237f59670e2d93b3fe3a6db95d3
Govind Anantwal <govindanantwal@gmail.com>
2021-05-23 12:17:49 +0530

    Second Commit

--- src/test/java/com/siyatech/app/SpringJenkinsExpApplicationTests.java
+++ src/test/java/com/siyatech/app/SpringJenkinsExpApplicationTests.java
@@ -1,13 +1,21 @@
 package com.siyatech.app;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringJenkinsExpApplicationTests {
 
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsExpApplicationTests.class);
 	@Test
-	void contextLoads() {
+	void contextLoads() 
+	{
+		logger.info("************Test case checked successfully*********************");
+		assertEquals(true, true);
 	}
 
 }
